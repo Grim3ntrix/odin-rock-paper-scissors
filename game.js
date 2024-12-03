4c563889393a90da0046620a3cf06bd0d9545351
Richard Samberi <richardsamberi101@gmail.com>
2024-12-03 18:30:27 +0800

    Add step-2 solution

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function getComputerChoice() {
+
+    let random = Math.floor(Math.random() * 3);
+
+    if (random === 1) {
+        return "rock";
+    } else if (random === 2) {
+        return "paper";
+    } else {
+        return "scissor";
+    }
+}
+
+console.log(getComputerChoice());
